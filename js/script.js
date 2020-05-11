1a427400588297fb19ebd948556ec07c65038130
ericafnunes <eriica.f.n@hotmail.com>
2020-05-11 18:16:06 -0300

    projeto do site maclarens

---
+++ js/script.js
@@ -0,0 +1,52 @@
+// $(document).ready(function () {
+	
+;(function ($) {
+	'use strict';
+
+  $(window).scroll(function () {
+    if ($('.navigation').offset().top > 100) {
+      $('.navigation').addClass('fixed-nav');
+    } else {
+      $('.navigation').removeClass('fixed-nav');
+    }
+  });
+  
+
+ $('.portfolio-gallery').each(function () {
+        $(this).find('.popup-gallery').magnificPopup({
+            type: 'image',
+            gallery: {
+                enabled: true
+            }
+        });
+	});
+	
+
+
+
+	// Init Magnific Popup
+	$('.portfolio-popup').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		gallery: {
+			enabled: true
+		},
+		mainClass: 'mfp-with-zoom',
+		navigateByImgClick: true,
+		arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>',
+		tPrev: 'Previous (Left arrow key)',
+		tNext: 'Next (Right arrow key)',
+		tCounter: '<span class="mfp-counter">%curr% of %total%</span>',
+		zoom: {
+			enabled: true,
+			duration: 300,
+			easing: 'ease-in-out',
+			opener: function (openerElement) {
+				return openerElement.is('img') ? openerElement : openerElement.find('img');
+			}
+		}
+	});
+
+
+
+})(jQuery);
